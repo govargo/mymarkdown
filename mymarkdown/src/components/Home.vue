165dcd79ab480a64fd56f0fe9b8c7582d293b73e
go_vargo <type.mafty@gmail.com>
2019-01-11 01:39:44 +0900

    Add Google Login and Logout feature

---
+++ mymarkdown/src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+        <button @click="googleLogin">Google アカウントでログイン</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+    data() {
+        return {
+            msg: "Welcome to MyMarkdown"
+        };
+    },
+    methods: {
+        googleLogin: function() {
+          firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+</style>
